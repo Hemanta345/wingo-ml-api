5c5eccb12701ca1e80fa0aee1a735968c3dc0000
Hemanta345 <117930797+Hemanta345@users.noreply.github.com>
2026-06-15 14:10:19 +0530

    main.py

--- main.py
+++ main.py
@@ -1,24 +1,13 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from collections import Counter
 
 app = FastAPI()
 
 class InputData(BaseModel):
     history: list[int]
 
-def size_of(n):
-    return "Big" if n >= 5 else "Small"
-
 @app.post("/predict")
 def predict(data: InputData):
-    sizes = [size_of(x) for x in data.history]
-
-    big_count = sizes.count("Big")
-    small_count = sizes.count("Small")
-
-    prediction = "Big" if big_count >= small_count else "Small"
-
     return {
-        "prediction": prediction
+        "prediction": "Big"
     }
